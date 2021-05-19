d41f3ac74ef610793c7e4e3810f22fc3f85320f9
Cassie <cassielevett@gmail.com>
2021-05-18 22:35:06 -0500

    rename files

--- module/forcoinblood2e.js
+++ module/forcoinblood2e.js
@@ -1,9 +1,9 @@
-import WeaponItemSheet from './module/sheets/WeaponItemSheet';
+import ForCoinBloodItemSheet from './module/sheets/ForCoinBloodItemSheet';
 
 Hooks.once("init", () => {
   const message = "Initialize For Coin and Blood 2nd Edition"
   console.log(`%c forcoinblood2e | ${message}`, "color: green; font-weight:bold");
 
   Items.unregisterSheet("core", ItemSheet)
-  Items.registerSheet("forcoinblood2e", WeaponItemSheet, { makeDefault: true })
+  Items.registerSheet("forcoinblood2e", ForCoinBloodItemSheet, { makeDefault: true })
 })
