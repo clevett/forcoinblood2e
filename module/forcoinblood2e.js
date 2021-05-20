f07ee7d6a1d5350612ce7dc4c6aa9f2e6b8ecf8e
Cassie <cassielevett@gmail.com>
2021-05-19 20:32:27 -0500

    fix typo that was breaking init

--- module/forcoinblood2e.js
+++ module/forcoinblood2e.js
@@ -1,8 +1,8 @@
-import ForCoinBloodItemSheet from './module/sheets/ForCoinBloodItemSheet';
+import ForCoinBloodItemSheet from './sheets/ForCoinBloodItemSheet.js';
 
-Hooks.once("init", () => {
+Hooks.once("init", function () {
   const message = "Initialize For Coin and Blood 2nd Edition"
-  console.log(`%c forcoinblood2e | ${message}`, "color: green; font-weight:bold");
+  console.log(`%c forcoinblood2e | ${message}`, "color:purple; font-weight:bold;");
 
   Items.unregisterSheet("core", ItemSheet)
   Items.registerSheet("forcoinblood2e", ForCoinBloodItemSheet, { makeDefault: true })
--- module/sheets/ForCoinBloodItemSheet.js
+++ module/sheets/ForCoinBloodItemSheet.js
@@ -1,5 +1,5 @@
 export default class ForCoinBloodItemSheet extends ItemSheet {
   get template() {
-    return `systems/forcoinblood2e/templates/${this.item.data.type}.html`
+    return `systems/forcoinblood2e/templates/sheets/${this.item.data.type}-sheet.html`
   }
 }
