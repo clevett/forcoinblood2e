15f846a87d5f07a82ba654cf5fd0be88e5f1dc2e
Cassie <cassielevett@gmail.com>
2021-05-21 15:46:07 -0500

    setup the sass

--- src/js/ForCoinBloodItemSheet.js
+++ src/js/ForCoinBloodItemSheet.js
@@ -1,6 +1,12 @@
-
-
 export default class ForCoinBloodItemSheet extends ItemSheet {
+  static get defaultOptions() {
+    return mergeObject(super.defaultOptions, {
+      width: 520,
+      height: 520,
+      classes: ["fcb2", "sheet", "item"]
+    })
+  }
+
   get template() {
     return `systems/forcoinblood2e/templates/${this.item.data.type}.html`
   }
